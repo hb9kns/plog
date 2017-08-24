7f8a8fee76427d3f47b39180a077dbe9355e7ec7
YargoA2 <yargo+git-asus2@jerq.org>
2017-08-25 00:17:45 +0200

    poorkyll.sh: add inclusion of stylefile

--- poorkyll.sh
+++ poorkyll.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# (2015,2016 YCB)
+# (2015,2017 YCB)
 # change bindir according to your needs!
 bindir=${BINDIR:-$HOME/bin}
 if test "$1" = ""
@@ -10,22 +10,29 @@ else if test -f "$1"
   echo ": using stylesheet $1"
  else cat <<EOU
  stylesheet '$1' missing!
- usage: $0 [<style>]
+ usage: $0 [<style>] [i]
   will generate files with .html suffix from all files with .md suffix,
   converting Markdown to HTML, and add a link to the stylesheet <style>,
-  or no stylesheet if argument is missing
+  or include <style> in output if trailing "i" is present,
+  or use no stylesheet if arguments are missing
  ( scripts mrkdwn.pl and convchars.sh must be present and executable
  in directory '$bindir' )
 EOU
  exit
  fi
+ case $2 in
+ [iI]*) sfl="<style> `cat \"$1\"` </style>"
+  echo ": including style in output"
+  ;;
+ *) echo ":: ignoring trailing argument $2" ;;
+ esac
 fi
 
 for mf in *.md
 do hf="${mf%md}html"
- echo -n ": $mf / $hf : "
+ echo ": $mf/html :"
  echo '<html><head><title>' >"$hf"
- grep '^#' "$mf" | head -n 1 | sed -e 's/#* *//' | tee -a "$hf"
+ grep '^#' "$mf" | head -n 1 | sed -e 's/#* */  /' | tee -a "$hf"
  echo '</title>' >>"$hf"
  echo "$sfl</head>" >>"$hf"
  echo '<body>' >>"$hf"
